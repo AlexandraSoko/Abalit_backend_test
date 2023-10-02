d4479d28e86968156d52afc6ded142f7d893b38c
Alexandra <sokolyanskaya@hotmail.com>
2023-10-02 15:55:47 +0200

    Creación de tablas de la DB

---
+++ Abalit_Web/database/migrations/2023_10_02_133554_create_usuarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuarios', function (Blueprint $table) {
+            $table->string('usuario');
+            $table->id();
+            $table->string('email')->unique();
+            $table->string('contraseña');
+            $table->string('tipo');
+            $table->string('caracteristicas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuarios');
+    }
+};
