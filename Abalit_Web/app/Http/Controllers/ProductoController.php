1b2ef768b907ca87fe103c4245c771951978e2b5
Alexandra <sokolyanskaya@hotmail.com>
2023-10-02 17:04:43 +0200

    Metodos login y singup

--- Abalit_Web/app/Http/Controllers/ProductoController.php
+++ Abalit_Web/app/Http/Controllers/ProductoController.php
@@ -3,8 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use app\Models\Usuario;
 
 class ProductoController extends Controller
 {
-    //
+    public function Login($email, $password) {
+        if ($this->validarCredenciales($email, $password)) {
+            // Autenticación exitosa
+            Redirect::route('inicio');
+        } else {
+            // Autenticación fallida
+            echo "Credenciales inválidas. Por favor, inténtalo de nuevo.";
+        }
+    }
+
+    private function validarCredenciales($email, $password)
+    {
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            // El usuario se autenticó correctamente
+            return true;
+        } else {
+            // Autenticación fallida
+            return false;
+        }
+    }
+
+    public function signup(Request $request)
+    {
+        // Valida los datos del formulario
+        $request->validate([
+            'nombre' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:Usuario|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        // Crea un nuevo usuario en la base de datos
+        $user = new Usuario;
+        $user->nombre = $request->input('nombre');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+
+        // Redirige a una página de inicio de sesión o muestra un mensaje de éxito
+        return redirect('/login')->with('success', '¡Registro exitoso! Por favor, inicia sesión.');
+    }
 }
--- Abalit_Web/config/auth.php
+++ Abalit_Web/config/auth.php
@@ -15,7 +15,7 @@ return [
 
     'defaults' => [
         'guard' => 'web',
-        'passwords' => 'users',
+        'passwords' => 'user',
     ],
 
     /*
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'user',
         ],
     ],
 
@@ -48,7 +48,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | All authentication drivers have a user provider. This defines how the
-    | users are actually retrieved out of your database or other storage
+    | user are actually retrieved out of your database or other storage
     | mechanisms used by this application to persist your user's data.
     |
     | If you have multiple user tables or models you may configure multiple
@@ -60,9 +60,9 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'user' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Usuario::class,
         ],
 
         // 'users' => [
